abf7241b03938ea5ce19321a484fe0ebab883314
october-22 <kaji.naohide@gmail.com>
2024-10-23 20:17:58 +0900

    cut slash

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-    base: 'https://diet-simulator-692917786e89.herokuapp.com/',
+    base: 'https://diet-simulator-692917786e89.herokuapp.com',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
