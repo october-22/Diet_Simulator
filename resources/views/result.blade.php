c05a09e6b77d7e7db9d04b7c84276954919fd470
october-22 <kaji.naohide@gmail.com>
2024-10-24 07:54:06 +0900

    not change

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -2,7 +2,7 @@
 
 <!DOCTYPE html>
 <html lang="ja">
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Result</title>
